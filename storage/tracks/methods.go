2d9a5d3db00218364eb456c79c27a194ca50fd58
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-07-01 18:14:40 +0500

    proto changed

--- storage/tracks/methods.go
+++ storage/tracks/methods.go
@@ -8,3 +8,11 @@ import (
 func(t *TrackRepo) Create(ctx context.Context, in *pb.Track) (pb.Void, error){
 	
 }
+
+
+Create(ctx context.Context, in *Composition) (*Void, error)
+Get(ctx context.Context, in *FilterRequest) (*CompositionsRes, error)
+GetByID(ctx context.Context, in *CompositionId) (*CompositionRes, error)
+GetByUserID(ctx context.Context, in *UserId) (*CompositionRes, error)
+Update(ctx context.Context, in *Composition) (*Void, error)
+Delete(ctx context.Context, in *CompositionId) (*Void, error)
